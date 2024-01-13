751d2ec8764782792a061b253798bf8f5d3215dc
Asroyx <mystoganza@hotmail.com>
2024-01-13 23:45:26 +0300

    Add friends state to App component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 function App() {
   const [name, setName] = useState("Asil");
   const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
 
   return (
     <div className="App">
       <h1>Hello! {name}</h1>
       <h2>{age}</h2>
+
       <button onClick={() => setName("Ahmet")}>Change name</button>
       <button onClick={() => setAge("25")}>Change age</button>
     </div>

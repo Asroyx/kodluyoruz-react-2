15337ac2df13bd4cdf8476dd0aa49361631ec6ce
Asroyx <mystoganza@hotmail.com>
2024-01-13 23:38:01 +0300

    Display age in App component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello! {name}</h1>
+      <h2>{age}</h2>
       <button onClick={() => setName("Ahmet")}>Click</button>
     </div>
   );

acba58d3ea9558da10ded422c0fdd0cd8b7b85f8
Asroyx <mystoganza@hotmail.com>
2024-01-13 23:40:39 +0300

    Add button to change age

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h1>Hello! {name}</h1>
       <h2>{age}</h2>
       <button onClick={() => setName("Ahmet")}>Change name</button>
+      <button onClick={() => setAge("25")}>Change age</button>
     </div>
   );
 }

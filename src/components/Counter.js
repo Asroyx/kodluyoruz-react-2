e71f16c89a51485e739d2f054bca09102071b97a
Asroyx <mystoganza@hotmail.com>
2024-01-18 23:45:32 +0300

    Implement increment functionality in Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,7 @@ function Counter() {
     <div>
       <h1>{count}</h1>
       <button>Decrase</button>
-      <button>Incrase</button>
+      <button onClick={() => setCount(count + 1)}>Incrase</button>
     </div>
   );
 }

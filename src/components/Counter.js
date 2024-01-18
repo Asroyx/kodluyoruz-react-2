bc1a371388a0b9df4c21db31bbe55a1bf2b88417
Asroyx <mystoganza@hotmail.com>
2024-01-18 23:47:51 +0300

    Refactor Counter component to use a separate function for incrementing

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+
+  const incrase = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={() => setCount(count - 1)}>Decrase</button>
-      <button onClick={() => setCount(count + 1)}>Incrase</button>
+      <button onClick={incrase}>Incrase</button>
     </div>
   );
 }

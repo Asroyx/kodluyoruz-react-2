861dd80dd9d57bb8ad5d9376771bad82db811874
Asroyx <mystoganza@hotmail.com>
2024-01-20 23:44:14 +0300

    Update root rendering and component import in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import Counter from "./components/Counter.js";
+// import Counter from "./components/Counter.js";
+import InputExample from "./components/InputExample";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
+    <InputExample />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

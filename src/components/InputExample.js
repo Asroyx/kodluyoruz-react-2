25c9bbafb2fca97765a2bed4b9eb52caa17330cb
Asroyx <mystoganza@hotmail.com>
2024-01-23 23:10:33 +0300

    Modify InputExample component to log input field name in onChangeInput handler

--- src/components/InputExample.js
+++ src/components/InputExample.js
@@ -4,18 +4,19 @@ function InputExample() {
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
 
-  const onChangeName = (event) => setName(event.target.value);
-  const onChangeSurname = (event) => setSurname(event.target.value);
+  const onChangeInput = (e) => {
+    console.log(e.target.name);
+  };
 
   return (
     <div>
       Please enter a name <br />
-      <input value={name} onChange={onChangeName} />
+      <input value={name} onChange={onChangeInput} />
       <br />
       <br />
       {name}
       Please enter a surname <br />
-      <input value={name} onChange={onChangeSurname} />
+      <input value={name} onChange={onChangeInput} />
       <br />
       {name} {surname}
     </div>
